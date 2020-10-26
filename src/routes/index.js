6d793ddee620542c0ba1a572347a6d81ae2af693
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2020-10-26 10:24:34 -0300

    Drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Stack from '../routes/Stack';
 import Tab from '../routes/Tabs';
+import Drawer from '../routes/Drawer';
 
 export default () => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
-      <Tab />
+      <Drawer />
     </NavigationContainer>
   </SafeAreaView>
 );
