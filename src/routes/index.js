3b6f2897ea03a2cc16ff1a07159e4e4b085b5f58
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2020-10-26 10:08:13 -0300

    Tab

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Stack from '../routes/Stack';
+import Tab from '../routes/Tabs';
 
 export default () => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
-      <Stack />
+      <Tab />
     </NavigationContainer>
   </SafeAreaView>
 );
--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -4,9 +4,17 @@ import TextoCentral from '../components/TextoCentral';
 // import { Container } from './styles';
 
 export default (props) => {
+  const numero =
+    props.route && props.route.params && props.route.params.numero
+      ? props.route.params.numero
+      : 0;
+  const numero2 =
+    props.route && props.route.params && props.route.params.numero2
+      ? props.route.params.numero2
+      : 0;
   return (
     <TextoCentral corFundo="#9932cd">
-      Tela C - {props.route.params.numero} - {props.route.params.numero2}
+      Tela C - {numero} - {numero2}
     </TextoCentral>
   );
 };
